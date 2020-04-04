895a89fd72d93a3a2f1170ca988c44e99b860f07
erocknine <echeungsk@gmail.com>
2020-04-04 19:29:37 -0400

    Fixed page numbers and buttons

--- js/index.js
+++ js/index.js
@@ -1,145 +1,150 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let pageNumber = 1; //initial page for renderPage()
 
-let pageNumber = 1 //initial page for renderPage()
+  const monstersDiv = document.querySelector(`#monster-container`);
+  const monsterForm = document.querySelector(`#create-monster`);
+  const buttonContainer = document.querySelector(`#page-buttons`);
+  const pageForm = document.querySelector(`#page-form`);
+  let pageText = document.querySelector(`.insert-page`);
+  pageText.value = `${pageNumber}`; //value for page number submit
 
-const monstersDiv = document.querySelector(`#monster-container`)
-const monsterForm = document.querySelector(`#create-monster`)
-const buttonContainer = document.querySelector(`#page-buttons`)
-const pageForm = document.querySelector(`#page-form`)
-let pageText = document.querySelector(`.insert-page`)
-pageText.value = `${pageNumber}` //value for page number submit
+  //render 20 monsters with page argument
+  function renderPage(pageNumber) {
+    monstersDiv.innerHTML = "";
+    fetch(`http://localhost:3000/monsters?_limit=20&_page=${pageNumber}`)
+      .then((response) => response.json())
+      .then((monsters) => {
+        if(monsters.length === 0) {
+          monstersDiv.innerHTML = `<h2>There are no monsters on this page</h2>`
+        } else {
+          monsters.forEach((monster) => renderMonster(monster));
+        }
+      });
+  }
 
-//render 20 monsters with page argument
-function renderPage (pageNumber) {
-  monstersDiv.innerHTML = ""
-  fetch(`http://localhost:3000/monsters?_limit=20&_page=${pageNumber}`)
-  .then(response => response.json())
-  .then(monsters => {
-    monsters.forEach(monster => 
-      renderMonster(monster))
-  })
-}
+  //render individual monster
+  function renderMonster({ name, age, description, id }) {
+    let monsterDiv = document.createElement(`div`);
+    monsterDiv.dataset.id = id;
+    monsterDiv.setAttribute("class", `monster-${name}`);
+    monsterDiv.innerHTML = `
+      <h2>${name}</h2>
+      <h4>${age}</h4>
+      <p><strong>Bio: </strong>${description}</p>
+      <button class="delete-button">Remove</delete>
+      `;
+    monstersDiv.append(monsterDiv);
+  }
 
-//render individual monster
-function renderMonster({name, age, description, id}) {
-  let monsterDiv = document.createElement(`div`)
-  monsterDiv.dataset.id = id
-  monsterDiv.setAttribute('class', `monster-${name}`)
-  monsterDiv.innerHTML = `
-  <h2>${name}</h2>
-  <h4>${age}</h4>
-  <p><strong>Bio: </strong>${description}</p>
-  <button class="delete-button">Remove</delete>
-  `
-  monstersDiv.append(monsterDiv)
-}
+  //render monster to top of page, for create
+  function renderMonsterPre({ name, age, description, id }) {
+    let monsterDiv = document.createElement(`div`);
+    monsterDiv.dataset.id = id;
+    monsterDiv.setAttribute("class", `monster-${name}`);
+    monsterDiv.innerHTML = `
+      <h2>${name}</h2>
+      <h4>${age}</h4>
+      <p><strong>Bio: </strong>${description}</p>
+      <button class="delete-button">Remove</delete>
+      `;
+    monstersDiv.prepend(monsterDiv);
+  }
 
-//render monster after create
-function renderMonsterPre({name, age, description, id}) {
-  let monsterDiv = document.createElement(`div`)
-  monsterDiv.dataset.id = id
-  monsterDiv.setAttribute('class', `monster-${name}`)
-  monsterDiv.innerHTML = `
-  <h2>${name}</h2>
-  <h4>${age}</h4>
-  <p><strong>Bio: </strong>${description}</p>
-  <button class="delete-button">Remove</delete>
-  `
-  monstersDiv.prepend(monsterDiv)
-}
+  //render monster to page
+  function renderForm() {
+    const form = document.createElement("form");
+    const name = document.createElement("input");
+    const age = document.createElement("input");
+    const description = document.createElement("input");
+    const button = document.createElement("button");
+    name.setAttribute("type", "text");
+    name.setAttribute("name", "name");
+    name.placeholder = "name...";
 
-//render monster to page
-function renderForm() {
-  const form = document.createElement('form')
-  const name = document.createElement('input')
-  const age = document.createElement('input')
-  const description = document.createElement('input')
-  const button = document.createElement('button')
-  name.setAttribute('type', 'text')
-  name.setAttribute('name', 'name')
-  name.placeholder = 'name...'
-  
-  age.setAttribute('type', 'text')
-  age.setAttribute('name', 'age')
-  age.placeholder = 'age...'
-  
-  description.setAttribute('type', 'text')
-  description.setAttribute('name', 'description')
-  description.placeholder = 'description...'
-  
-  button.setAttribute('type', 'submit')
-  button.textContent = 'Create monster'
+    age.setAttribute("type", "text");
+    age.setAttribute("name", "age");
+    age.placeholder = "age...";
 
-  form.append(name)
-  form.append(age)
-  form.append(description)
-  form.append(button)
-  monsterForm.append(form)
-}
+    description.setAttribute("type", "text");
+    description.setAttribute("name", "description");
+    description.placeholder = "description...";
 
-//delete monster function called from delete button event
-function deleteMonster(monster) {
-  fetch(`http://localhost:3000/monsters/${monster.dataset.id}`, {
-    method: 'DELETE'
-  })
-  .then(response => {
-    if(response.ok){
-      monster.remove()
+    button.setAttribute("type", "submit");
+    button.textContent = "Create monster";
+
+    form.append(name);
+    form.append(age);
+    form.append(description);
+    form.append(button);
+    monsterForm.append(form);
+  }
+
+  //delete monster function called from delete button event
+  function deleteMonster(monster) {
+    fetch(`http://localhost:3000/monsters/${monster.dataset.id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        monster.remove();
+      }
+    });
+  }
+
+  //delete monster event for button
+  monstersDiv.addEventListener("click", (event) => {
+    if (event.target.className === "delete-button") {
+      deleteMonster(event.target.parentElement);
     }
-  })
-}
+  });
 
-//delete monster
-monstersDiv.addEventListener('click', event => {
-  if(event.target.className === "delete-button"){
-    deleteMonster(event.target.parentElement)
-  }
-})
+  //create monster with form
+  monsterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const monster = {
+      name: event.target.name.value,
+      age: event.target.age.value,
+      description: event.target.description.value,
+    };
+    fetch("http://localhost:3000/monsters", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(monster),
+    })
+      .then((response) => response.json())
+      .then((monster) => {
+        renderMonsterPre(monster);
+      });
+  });
 
-//create monster with form
-monsterForm.addEventListener("submit", event => {
-  event.preventDefault()
-  const monster = {
-    name: event.target.name.value,
-    age: event.target.age.value,
-    description: event.target.description.value
-  }
-  fetch('http://localhost:3000/monsters', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(monster),
-  })
-  .then(response => response.json())
-  .then(monster => {
-    renderMonsterPre(monster)
-  })
-})
+  //turn pages with forward & back buttons
+  buttonContainer.addEventListener("click", (event) => {
+    if (event.target.className === "forward") {
+      pageNumber += 1;
+      monstersDiv.innerHTML = "";
+      renderPage(pageNumber);
+      pageText.value = pageNumber;
+    } else if (event.target.className === "back") {
+      pageNumber -= 1;
+      if(pageNumber < 1){
+        pageNumber = 1
+      }
+      monstersDiv.innerHTML = "";
+      renderPage(pageNumber);
+      pageText.value = pageNumber;
+    }
+  });
 
-//turn pages
-buttonContainer.addEventListener('click', event => {
-  if(event.target.className === "forward") {
-    pageNumber += 1
-    monstersDiv.innerHTML = ""
-    renderPage(pageNumber)
-    pageText.value = pageNumber
-  } else if(event.target.className === "back") {
-    pageNumber -= 1
-    monstersDiv.innerHTML = ""
-    renderPage(pageNumber)
-    pageText.value = pageNumber
-  }
-})
+  //page number submit event
+  pageForm.addEventListener("click", (event) => {
+    if (event.target.className === "submit") {
+      let pageInput = event.target.parentElement.querySelector(".insert-page");
+      pageNumber = pageInput.value
+      renderPage(pageNumber);
+    }
+  });
 
-//page number submit event
-pageForm.addEventListener('click', event => {
-  if(event.target.className === "submit") {
-    let pageInput = event.target.parentElement.querySelector('.insert-page')
-    renderPage(pageInput.value)
-  }
-})
-
-renderForm()
-renderPage(1)
+  renderForm();
+  renderPage(1);
 });
